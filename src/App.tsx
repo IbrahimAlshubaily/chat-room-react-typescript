77fc4b73f6daa0701e7c9326dcfe7a8d956c9934
IbrahimAlshubaily <ibra.94@live.com>
2022-10-03 14:36:46 +0300

    improve style

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import React from 'react';
+import './App.css'
 
 export default function App() {
 
   return (
     <div className="App"> 
-    {MessagesForm()}
-    {InputField()} 
-    {SubmitButton()}
+      {MessagesForm()}
+      <div id='user-input'>
+        {InputField()} 
+        {SubmitButton()}
+      </div>
     </div>
   );
 }
 
 function MessagesForm() : JSX.Element {
-  return <ul id='messages' className='messages-form'>X</ul>
+  return <ul id='messages' className='messages-form'></ul>
 }
 
 function InputField() : JSX.Element {
-  return <input id='user-input' className='input-field'></input>
+  return <input id='text-box' className='input-field' onKeyDown={(e) => {
+    if (e.key === "Enter") {
+      document.getElementById('submit-button')?.click()
+    }
+  }}/>
 }
 
 function SubmitButton() : JSX.Element {
-  return <button onClick={updateMessageForm}>Submit</button>
+  return <button id='submit-button' onClick={updateMessageForm}>Submit</button>
 }
 
 function updateMessageForm() {
-  const text = document.getElementById('user-input') as HTMLInputElement;
+  const text = document.getElementById('text-box') as HTMLInputElement;
   if (text.value){
     const messages = document.getElementById('messages') as HTMLUListElement
-    messages.append(text.value);
+    const li = document.createElement('li')
+    li.textContent = text.value;
+    messages.append(li);
     text.value = '';
   }
-
 }
