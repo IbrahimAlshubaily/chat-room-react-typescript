1a2ff3b8f803a626b5dacf84c38d3302c4699729
IbrahimAlshubaily <ibra.94@live.com>
2022-10-03 13:30:02 +0300

    working solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    {MessagesForm()}
+    {InputField()} 
+    {SubmitButton()}
     </div>
   );
 }
 
-export default App;
+function MessagesForm() : JSX.Element {
+  return <ul id='messages' className='messages-form'>X</ul>
+}
+
+function InputField() : JSX.Element {
+  return <input id='user-input' className='input-field'></input>
+}
+
+function SubmitButton() : JSX.Element {
+  return <button onClick={updateMessageForm}>Submit</button>
+}
+
+function updateMessageForm() {
+  const text = document.getElementById('user-input') as HTMLInputElement;
+  if (text.value){
+    const messages = document.getElementById('messages') as HTMLUListElement
+    messages.append(text.value);
+    text.value = '';
+  }
+
+}
